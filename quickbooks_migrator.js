570a25c84b10ba24ca393fb145864a9e0a198462
OliveERP <120718232+OliveERP@users.noreply.github.com>
2023-01-20 12:38:24 +0500

    updating the client script

--- quickbooks_migrator.js
+++ quickbooks_migrator.js
@@ -2,6 +2,11 @@ frappe.ui.form.on('QuickBooks Migrator', {
 	refresh(frm) {
 		if (frm.doc.access_token) {
 			if (frm.doc.company) {
+				frm.add_custom_button(__("Fetch Data"), function () {
+					frm.trigger("fetch_data")
+					frm.set_value("last_fetched", frappe.datetime.now_datetime())
+					frm.save()
+				});
 				frm.add_custom_button(__("Sync Data"), function () {
 					frm.call({
 						"method":"qb_post",
@@ -9,6 +14,7 @@ frappe.ui.form.on('QuickBooks Migrator', {
 						callback: function(r) {
 							console.log(r.message);
 							frm.set_value("status", "Complete");
+							frm.set_value("last_synced", frappe.datetime.now_datetime())
 							frm.save();
 						}
 					});
